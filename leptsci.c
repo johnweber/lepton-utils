ee8ceb921b31fba39753ef7ee6efdb04b0f40fac
John Weber <rjohnweber@gmail.com>
2015-06-14 20:32:12 -0500

    leptsci:  General improvements

--- leptsci.c
+++ leptsci.c
@@ -5,11 +5,27 @@
 #include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
 #include <stdint.h>
+#include "leptsci.h"
 
 static uint8_t bits = 8;
-static uint32_t speed = 16000000;       //16
+static uint32_t speed = 20000000;
 static uint16_t delay = 0;
 static int leptfd;
+#define VOSPI_FRAME_SIZE 164
+#define LEPTON_NUM_ROWS 60
+#define LEPTON_NUM_COLUMNS 80
+
+#define PRINT_DEBUG_INFO 0
+#define PRINT_TIMING_INFO 0
+
+// At 16MHz, a 164-byte message transfer takes about 82 microseconds
+// The Lepton has a new frame about every 1/27th second (about 37 milliseconds)
+// We need to check for a new at about 3x that rate (every 1 milliseconds).
+#define VOSPI_FRAME_DELAY_MICROSECONDS 1000
+
+// Re-sync delay time.  This is the time recommended by FLIR to deassert CS
+// and the SPI CLK in order to wait for resync
+#define VOSPI_FRAME_RESYNC_TIME_MICROSECONDS 200000
 
 int leptopen()
 {
@@ -33,66 +49,115 @@ int leptopen()
     return 0;
 }
 
-void leptclose()
+int leptclose()
 {
     close(leptfd);
+    return 0;
+}
+
+double time_subtract(struct timespec ts_old_time, struct timespec ts_time) {
+
+	double old_time = (double)ts_old_time.tv_sec + (double)ts_old_time.tv_nsec/(double)1E9;
+	double new_time = (double)ts_time.tv_sec + (double)ts_time.tv_nsec/(double)1E9;
+
+	return new_time - old_time;
 }
 
 int leptget(unsigned short *img)
 {
-#define VOSPI_FRAME_SIZE (164)
     int row = -1;
-    int hiccup = 0;
-    int notready = 0;
+    static int sync = 1;   // Trigger sync in first transfer
+    int capturing_frame = 0;
+    struct timespec ts_capture_start, ts_capture_end;
+    struct timespec ts_transfer_start, ts_transfer_end;
+    double total_transfer_time = 0.0;
+    int num_transfers = 0;
+
     do {
 
+    	if(sync) {
+
+    		// Wait for an interval to ensure a timeout of the VoSPI
+    		// interface
+    		usleep(VOSPI_FRAME_RESYNC_TIME_MICROSECONDS);
+
+    		// Let's not do this again unless we have to
+    		sync = 0;
+    	}
+
         uint8_t lepacket[VOSPI_FRAME_SIZE];
-        uint8_t tx[VOSPI_FRAME_SIZE] = { 0, };
         struct spi_ioc_transfer tr = {
-            .tx_buf = (unsigned long) tx,
+            .tx_buf = (unsigned long) NULL,      // NULL if not transmitting
             .rx_buf = (unsigned long) lepacket,
             .len = VOSPI_FRAME_SIZE,
             .delay_usecs = delay,
             .speed_hz = speed,
             .bits_per_word = bits,
+            .cs_change = 0,
         };
 
-        int i;
-        i = ioctl(leptfd, SPI_IOC_MESSAGE(1), &tr);
-        if (i < 1) {
-            fprintf(stderr, "1");
-            continue;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts_transfer_start);
+
+        int ret = ioctl(leptfd, SPI_IOC_MESSAGE(1), &tr);
+        if (ret < 1) {  // could not transfer
+        	fprintf(stderr, "Could not complete SPI transfer\n");
+        	return -1;
         }
-        //return -1;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts_transfer_end);
+
+        total_transfer_time += time_subtract(ts_transfer_start, ts_transfer_end);
+        num_transfers++;
+
+        // Check for dummy frame
         if (((lepacket[0] & 0xf) == 0x0f)) {
-            //fprintf( stderr, "." );
-            if (!notready)
-                usleep(25000);  // wait for next frame
-            else
-                usleep(1000);
-            notready++;
+        	usleep(VOSPI_FRAME_DELAY_MICROSECONDS);
+        	if(PRINT_DEBUG_INFO) printf( "d");
             continue;
         }
-        //if( notready ) fprintf( stderr, "<%d>", notready );
-        notready = 0;
-        //return -2;
+
+        // We are receiving valid packets.
         row = lepacket[1];
-        hiccup++;
-        if (row >= 60) {
-            if (hiccup > 8) {
-                //leptclose();
-                usleep(125000);
-                //leptopen();
-                fprintf(stderr, "!\n");
-            }
-            fprintf(stderr, "[%d]", row);
+
+        // Check to see if row is out of bounds, which
+        // indicates an error (as there can only be a max of 60 rows)
+        // TODO:  This should be investigated further as this condition
+        // should not happen.
+        if (row >= LEPTON_NUM_ROWS) {
+
+        	printf("[%d]\n", row);
+
+            // Trigger resync at the start of the next loop
+            sync = 1;
+            row = -1;
             continue;
         }
-        hiccup = 0;
-        for (i = 0; i < 80; i++)
-            img[row * 80 + i]
+
+        if(row == 0 && capturing_frame == 0){  // Packet is first line, grab a timestamp
+        	capturing_frame = 1;
+        	clock_gettime(CLOCK_MONOTONIC_RAW, &ts_capture_start);
+        }
+
+        // Transfer packet data to img.
+        int i;
+        for ( i = 0; i < LEPTON_NUM_COLUMNS; i++) {
+            img[row * LEPTON_NUM_COLUMNS + i]
               = (lepacket[2 * i + 4] << 8) | lepacket[2 * i + 5];
-        //printf ("%d ",img[row*81]);
-    } while (row != 59);
+        }
+
+        if(PRINT_DEBUG_INFO) printf("p%d",row);
+
+    } while (row < LEPTON_NUM_ROWS - 1);
+
+    // Finalized the frame, grab the time
+    clock_gettime(CLOCK_MONOTONIC_RAW, &ts_capture_end);
+
+    if(PRINT_DEBUG_INFO) printf("f\n");
+
+    double frame_time;
+    if(PRINT_TIMING_INFO) {
+    	printf("leptget: image capture: = %f ", time_subtract(ts_capture_start, ts_capture_end)*(double)1e6);
+    	printf("# xfers: %d, avg xfer time %f\n", num_transfers, (total_transfer_time/(double)num_transfers)*(double)1e6);
+    }
+
     return 0;
 }
--- leptsci.h
+++ leptsci.h
@@ -1,3 +1,13 @@
+#ifndef __LEPTSCI_H
+#define __LEPTSCI_H
+
+#include <sys/time.h>
+#include <time.h>
+
 int leptopen(void);
 int leptget(unsigned short *);
 int leptclose(void);
+double time_subtract(struct timespec ts_old_time, struct timespec ts_time);
+
+
+#endif
